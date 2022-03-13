ed3dd8d9b46330e5f03d1677e86c1db693937fb2
ashwin271 <ashwinmurali27@gmail.com>
2022-03-13 22:35:12 +0530

    3rd problem solved

--- largest prime factor.py
+++ largest prime factor.py
@@ -1,7 +1,26 @@
-import math
+def primenumber(n):
+    op=True
+    l=int(n**0.50)+1
+    for i in range (2,l):
+        if n%i==0:
+            op=False
+            break
+    return op
 
 x=600851475143
-l
+factors=[]
+lpf=0
 
-while True :
+for i in range (1,int(x**0.50)+1):
+    if x%i==0:
+        factors.append(i)
+        factors.append(int(x/i))
+
+for j in factors:
+    if primenumber(j):
+        if j>lpf:
+            lpf=j
+
+print(lpf)
+        
     
