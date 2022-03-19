e230ed6cf5661310d62674d2ce91fd7dacccc11e
ashwin271 <ashwinmurali27@gmail.com>
2022-03-19 08:00:51 +0530

    9th question

---
+++ special pythagorean triplet.py
@@ -0,0 +1,17 @@
+def spepytrip(n):
+    op = False
+    u=int(n/2)
+    for i in range (1,u):
+        for j in range (i,u):
+            k=((i**2)+(j**2))**0.5
+            if k-int(k)==0 :
+                if i+j+k==n:
+                    print(i*j*k)
+                    op=True
+                    break
+        if op==True:
+            break
+
+spepytrip(1000)
+            
+    
